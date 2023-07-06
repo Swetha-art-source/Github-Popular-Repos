fddf4f081e525a330584346e1850c85a3b773f9c
swetha <buddiswetha12@gamail.com>
2023-07-06 04:30:30 +0000

    Popular Github Repos

---
+++ src/components/LanguageFilterItem/index.js
@@ -0,0 +1,28 @@
+// Write your code here
+import './index.css'
+
+const LanguageFilterItem = props => {
+  const {eachItem, isActiveId, activeIsChance} = props
+  const {language, id} = eachItem
+
+  const onActiveIdChange = () => {
+    activeIsChance(id)
+  }
+
+  const activeBtn = isActiveId ? 'activeButton' : ''
+  const activeText = isActiveId ? 'activeText' : ''
+
+  return (
+    <li className="header-li-container">
+      <button
+        onClick={onActiveIdChange}
+        type="button"
+        className={`nav-button ${activeBtn} `}
+      >
+        <p className={`header-name ${activeText}`}>{language}</p>
+      </button>
+    </li>
+  )
+}
+
+export default LanguageFilterItem
